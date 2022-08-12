5033af351cd46ba9e9d4dc9ee61079a92a2752ce
Tim <58736434+software-made-easy@users.noreply.github.com>
2022-08-12 15:09:49 +0200

    Delete scripts directory

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-# change directory to the scripts parent directory
-cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/..
-
-
-mkdir build && cd build # create build directory
-cmake .. # create Makefile
-cmake --build . -j4 # build project
-
--- scripts/updateTranslations.sh
+++
@@ -1,5 +0,0 @@
-# change directory to the scripts parent directory
-cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/..
-
-# Update translations using lupdate
-lupdate src/*.cpp ui/mainwindow.ui -ts translations/Speech_de_DE.ts -noobsolete
